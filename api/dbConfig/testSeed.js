1c15678ff6fe41817900d5709a5bd59f8f83fff6
James Wheadon <james.wheadon@yahoo.com>
2021-03-30 16:26:04 +0100

    fix errors in docker-compose and dbConfig

--- api/dbConfig/testSeed.js
+++ api/dbConfig/testSeed.js
@@ -1,6 +1,6 @@
 const db = require('./init');
 const fs = require('fs');
 
-const seeds = fs.readFileSync(__dirname + 'test_seeds.sql').toString();
+const seeds = fs.readFileSync(__dirname + '/test_seeds.sql').toString();
 
 db.query(seeds, () => console.log("Dev database seeded"));
