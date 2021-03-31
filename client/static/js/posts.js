823fd5f71a5540ee78f950b88bfb6e7f55a06d66
HeyAero <aeroyts@icloud.com>
2021-03-31 10:26:45 +0100

    update styling and refactor display post

--- client/static/js/posts.js
+++ client/static/js/posts.js
@@ -1,17 +1,13 @@
 // Displaying a post
 async function loadPost(hash) {
-    document.getElementById('postName').textContent = "";
     document.getElementById('postTitle').textContent = "";
-    document.getElementById('postDate').textContent = "";
     document.getElementById('postBody').textContent = "";
     let postData = await getPost(hash);
     console.log(postData);
     if (postData.err) {
         document.getElementById('postName').textContent = "Error post not found";
     } else {
-        document.getElementById('postTitle').textContent = postData.title;
-        document.getElementById('postName').textContent = postData.name;
-        document.getElementById('postDate').textContent = postData.date;
+        document.getElementById('postTitle').textContent = `${postData.date} > ${postData.name} > ${postData.title}`;
         document.getElementById('postBody').textContent = postData.body;
     }
 }
